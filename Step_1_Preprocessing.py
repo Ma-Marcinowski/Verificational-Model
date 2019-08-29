aa0970145a020a81246ee6be79dd53118c573a5a
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2019-08-29 14:15:19 +0200

    Preprocessing v3.0

--- Step_1_Preprocessing.py
+++ Step_1_Preprocessing.py
@@ -7,8 +7,8 @@ tifs = glob.glob('*.tif')
 
 for j in tqdm(tifs):
 
-    img = cv2.imread(j, 0)
-    inv = np.bitwise_not(img)
+    img = cv2.imread(j, 0) #Load image in grayscale.
+    inv = np.bitwise_not(img) #Image colors invert (because of high background value, i.e. white=255 to black=0).
 
     y=890
     x=323
@@ -29,7 +29,7 @@ for j in tqdm(tifs):
 
             mean = v.mean()
 
-            if mean >= 3:
+            if mean >= 4: #Arbitrary threshold of mean image pixel values, to exclude empty and half-empty images.
 
                 cv2.imwrite('/path/' + j[:-4] + '-' + str(idx) + str(ind) + '.png', v)
 
