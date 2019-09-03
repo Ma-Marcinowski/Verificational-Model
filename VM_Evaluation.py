747b65876da13e87273138b5eaa608227470e7af
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2019-09-03 18:04:05 +0200

    VM (Siamese CNN, tf.keras API) v2.0

--- VM_Evaluation.py
+++ VM_Evaluation.py
@@ -6,7 +6,7 @@ from tensorflow.keras import losses
 from tensorflow.keras.utils import Sequence
 from tensorflow.keras.models import Model, load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.layers import Input, Activation, Dense, Conv2D, MaxPooling2D, Dropout, Flatten
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten, BatchNormalization, Dropout 
 
 def load_image(img):
     return img_to_array(load_img(img, color_mode='grayscale')) / 255.
