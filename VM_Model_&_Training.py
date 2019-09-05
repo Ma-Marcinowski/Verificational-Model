edcfd6ca2f598744b84596b5932665ee8f9b54ae
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2019-09-05 22:44:56 +0200

    VM (Siamese CNN, tf.keras API) v2.0

--- VM_Model_&_Training.py
+++ VM_Model_&_Training.py
@@ -3,10 +3,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from tensorflow.keras import losses
+from tensorflow.keras.losses import binary_crossentropy 
 from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dense
+from tensorflow.keras.layers import Flatten, BatchNormalization, Dropout 
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Dense, Flatten, BatchNormalization, Dropout 
 
 def load_image(img):
     return img_to_array(load_img(img, color_mode='grayscale')) / 255.
