4e56150f5a02b2be8acfc42f868a27a56e2e59cd
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2019-09-06 23:45:11 +0200

    VM Core Network v2.0

--- VM_AlexNet_v2.py
+++ VM_AlexNet_v2.py
@@ -9,9 +9,11 @@ right_input = Input(shape=(256, 256, 1), name='right_input')
 xl = Conv2D(96, (11, 11), strides=4, padding='same', activation='relu', input_shape=(256, 256, 1), name='1stConvLeft')(left_input)
 xl = BatchNormalization(axis=-1, scale=True, trainable=True)(xl)
 xl = MaxPooling2D(pool_size=(3, 3), strides=2, padding='valid', name='1stPoolLeft')(xl)
+
 xl = Conv2D(256, (5, 5), strides=1, padding='same', activation='relu', name='2ndConvLeft')(xl)
 xl = BatchNormalization(axis=-1, scale=True, trainable=True)(xl)
 xl = MaxPooling2D(pool_size=(3, 3), strides=2, padding='valid', name='2ndPoolLeft')(xl)
+
 xl = Conv2D(384, (3, 3), strides=1, padding='same', activation='relu', name='3rdConvLeft')(xl)
 xl = BatchNormalization(axis=-1, scale=True, trainable=True)(xl)
 xl = Conv2D(384, (3, 3), strides=1, padding='same', activation='relu', name='4thConvLeft')(xl)
@@ -23,9 +25,11 @@ left_out = MaxPooling2D(pool_size=(3, 3), strides=2, padding='valid', name='3rdP
 xr = Conv2D(96, (11, 11), strides=4, padding='same', activation='relu', input_shape=(256, 256, 1), name='1stConvRight')(right_input)
 xr = BatchNormalization(axis=-1, scale=True, trainable=True)(xr)
 xr = MaxPooling2D(pool_size=(3, 3), strides=2, padding='valid', name='1stPoolRight')(xr)
+
 xr = Conv2D(256, (5, 5), strides=1, padding='same', activation='relu', name='2ndConvRight')(xr)
 xr = BatchNormalization(axis=-1, scale=True, trainable=True)(xr)
 xr = MaxPooling2D(pool_size=(3, 3), strides=2, padding='valid', name='2ndPoolRight')(xr)
+
 xr = Conv2D(384, (3, 3), strides=1, padding='same', activation='relu', name='3rdConvRight')(xr)
 xr = BatchNormalization(axis=-1, scale=True, trainable=True)(xr)
 xr = Conv2D(384, (3, 3), strides=1, padding='same', activation='relu', name='4thConvRight')(xr)
