5647fb97187b6ba0e53e4a304da24b6647319fc6
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2019-09-06 23:09:08 +0200

    VM Core Network v1.1

--- VM_AlexNet.py
+++ VM_AlexNet.py
@@ -59,4 +59,4 @@ model = Model(inputs=[left_input, right_input], outputs=[output])
 model.summary(line_length=None, 
               positions=None,
               print_fn=None)
-tf.keras.utils.plot_model(model, to_file='/content/VM_AlexNet.png', show_shapes=True, show_layer_names=True, rankdir='TB')
+tf.keras.utils.plot_model(model, to_file='/content/VM_AlexNet_v11.png', show_shapes=True, show_layer_names=True, rankdir='TB')
