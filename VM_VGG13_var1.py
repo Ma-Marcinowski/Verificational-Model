13c40f4567e7e07c69ac6b5a6a2d17c12fbc3056
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2019-09-06 23:59:33 +0200

    VM Core Network var1.0

--- VM_VGG13_var1.py
+++ VM_VGG13_var1.py
@@ -95,4 +95,4 @@ model = Model(inputs=[left_input, right_input], outputs=[output])
 model.summary(line_length=None, 
               positions=None,
               print_fn=None)
-tf.keras.utils.plot_model(model, to_file='/content/VM_VGG13_v1.png', show_shapes=True, show_layer_names=True, rankdir='TB')
+tf.keras.utils.plot_model(model, to_file='/content/VM_VGG13_var1.png', show_shapes=True, show_layer_names=True, rankdir='TB')
