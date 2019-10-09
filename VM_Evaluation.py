c49f553db1bfbed1e023301b054ec350e95bf099
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2019-10-09 18:47:10 +0200

    VM (Siamese CNN, tf.keras API) v3.0

--- VM_Evaluation.py
+++ VM_Evaluation.py
@@ -63,8 +63,6 @@ AUC = tf.keras.metrics.AUC()
 
 model.compile(optimizer=Adam, loss='binary_crossentropy', metrics=['accuracy', TPR, TN, FP, FN, PPV, AUC])
 
-csv_logger = tf.keras.callbacks.CSVLogger('/path/TestLog.csv', separator=',', append=False)
-
 tensorboard = tf.keras.callbacks.TensorBoard(log_dir='/path/TestLogs/',
                                              histogram_freq=1,
                                              batch_size=BatchSize,
@@ -74,7 +72,7 @@ tensorboard = tf.keras.callbacks.TensorBoard(log_dir='/path/TestLogs/',
                                              update_freq='batch')
 
 evaluation = model.evaluate_generator(generator=TestSeq,
-                                      callbacks=[csv_logger, tensorboard],
+                                      callbacks=[tensorboard],
                                       use_multiprocessing=False,
                                       max_queue_size=10,
                                       workers=1,
