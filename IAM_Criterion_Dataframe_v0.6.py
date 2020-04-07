8768740032d93e540c5287c19ac168063fc79ad7
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2020-04-07 18:53:51 +0200

    Criteria Dataframes v0.6 Example

---
+++ IAM_Criterion_Dataframe_v0.6.py
@@ -0,0 +1,81 @@
+import glob
+import os
+import csv
+import random
+import sklearn
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+
+def Dataframe(img_path, df_path, df_img_path):
+
+    os.chdir(img_path)
+
+    pngs = glob.glob('*.png')
+
+    with open(df_path, 'a+') as f:
+
+        writer = csv.writer(f)
+
+        rev = set()
+
+        pos = 0
+
+        for j in tqdm(pngs, leave=False):
+            for i in pngs:
+
+                if j[:8] == i[:8] and j[0] in ('i') and i[0] in ('i') and (j, i) not in rev:
+
+                    pair = [df_img_path + j, df_img_path + i, 1]
+
+                    writer.writerow(pair)
+
+                    pos += 1
+
+                    rev.add((i, j))
+
+                else:
+
+                    continue
+
+        print('Done IAM criterion positives: ', pos, ' instances.')
+
+        neg = 0
+
+        while neg < pos:
+
+            j = random.choice(pngs)
+            i = random.choice(pngs)
+
+            if j[:8] != i[:8] and j[0] in ('i') and i[0] in ('i') and (j, i) not in rev:
+
+                pair = [df_img_path + j, df_img_path + i, 0]
+
+                writer.writerow(pair)
+
+                neg += 1
+
+                rev.add((i, j))
+
+                print('%.2f%%'%(100*neg/pos), end="\r")
+
+            else:
+
+                continue
+
+        else:
+
+            print('Done IAM criterion negatives: ', neg, ' instances.')
+
+    df = pd.read_csv(df_path, header=None)
+
+    df = sklearn.utils.shuffle(df)
+
+    df.to_csv(df_path, header=['Leftname', 'Rightname', 'Label'], index=False)
+
+    print('Done IAM criterion dataframe: ', df.shape[0], ' image pairs.')
+
+IAM_Criterion_Dataframe = Dataframe(img_path='/preprocessed/test/images/directory/',
+                                    df_path='/dataframe/save/directory/IAM_Criterion_Dataframe.csv',
+                                    df_img_path='/preprocessed/test/images/directory/indicated/in/the/test/dataframe')
---
+++ Negative_Criterion_Dataframe_v0.6.py
@@ -0,0 +1,61 @@
+import glob
+import os
+import csv
+import random
+import sklearn
+import numpy as np
+import pandas as pd
+
+from tqdm import tqdm
+
+def Dataframe(img_path, df_path, df_img_path):
+
+    os.chdir(img_path)
+
+    pngs = glob.glob('*.png')
+
+    with open(df_path, 'a+') as f:
+
+        writer = csv.writer(f)
+
+        rev = set()
+
+        n = 200000
+        neg = 0
+
+        while neg < n:
+
+            j = random.choice(pngs)
+            i = random.choice(pngs)
+
+            if j[:8] != i[:8] and j[0] in ('c') and i[0] in ('i') and (j, i) not in rev:
+
+                pair = [df_img_path + j, df_img_path + i, 0]
+
+                writer.writerow(pair)
+
+                neg += 1
+
+                rev.add((i, j))
+
+                print('%.2f%%'%(100*neg/n), end="\r")
+
+            else:
+
+                continue
+
+        else:
+
+            print('Done negative criterion negatives: ', neg, ' instances.')
+
+    df = pd.read_csv(df_path, header=None)
+
+    df = sklearn.utils.shuffle(df)
+
+    df.to_csv(df_path, header=['Leftname', 'Rightname', 'Label'], index=False)
+
+    print('Done Negative Criterion dataframe: ', df.shape[0], ' image pairs.')
+
+Negative_Criterion_Dataframe = Dataframe(img_path='/preprocessed/test/images/directory/',
+                                         df_path='/dataframe/save/directory/Negative_Criterion_Dataframe.csv',
+                                         df_img_path='/preprocessed/test/images/directory/indicated/in/the/test/dataframe')
