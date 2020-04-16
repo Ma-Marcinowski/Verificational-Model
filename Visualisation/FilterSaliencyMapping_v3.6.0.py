c83343173446788680344f4863aafe0ea85f0f47
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2020-04-16 14:49:54 +0200

    Filters Saliency Maps v3.6.0 example (tf. v1.14)

--- Visualisation/FilterSaliencyMapping_v3.6.0.py
+++ Visualisation/FilterSaliencyMapping_v3.6.0.py
@@ -20,22 +20,24 @@ def InitializeImage(img_path):
 def FeatureGradients(image, feature_index, feature_layer):
 
     with tf.GradientTape() as tape:
-
+      
         tape.watch(image)
 
         feature_layer_output = feature_layer(image)
 
-        given_feature_value = feature_layer_output[:, feature_index] #Globally average pooled activation value
-
+        given_feature_value = feature_layer_output[:, feature_index]
+        
     feature_gradients = tape.gradient(given_feature_value, image)
 
-    positive_gradients = tf.nn.relu(feature_gradients)
+    positive_gradients = tf.clip_by_value(feature_gradients, clip_value_min=0, clip_value_max=255)
+    negative_gradients = tf.clip_by_value(feature_gradients, clip_value_min=-255, clip_value_max=0)
 
-    small_gradients = tf.math.divide(1, tf.math.add(positive_gradients, 1e-9))
+    small_postivie_gradients = tf.math.divide(1, tf.math.add(positive_gradients, 1e-9))
+    small_negative_gradients = tf.math.divide(1, tf.math.add(negative_gradients, 1e-9))
 
-    return small_gradients, given_feature_value
+    return small_postivie_gradients, small_negative_gradients, given_feature_value
 
-def FilterBackpropSaliencyMapping(model_load_path, img_out_path, images_dir, input_layer_name, output_layer_name, num_of_features):
+def BackpropSaliencyMapping(model_load_path, img_out_path, images_dir, input_layer_name, output_layer_name, num_of_features):
 
     model = load_model(model_load_path)
 
@@ -50,27 +52,40 @@ def FilterBackpropSaliencyMapping(model_load_path, img_out_path, images_dir, inp
     for img_path, img_name in tqdm(images, total=len(image_paths), desc=output_layer_name + ' features mapping:', leave=True):
 
         raw_image, initial_image = InitializeImage(img_path=img_path)
-        empty_img = np.zeros(raw_image.shape, dtype=np.float32)
+        #empty_img = np.zeros(raw_image.shape, dtype=np.float32)
 
         for feature_index in tqdm(range(num_of_features), desc='Image features mapping:', leave=False):
 
-            feature_gradients, feature_value = FeatureGradients(image=initial_image,
-                                                                feature_index=feature_index,
-                                                                feature_layer=feature_layer)
+            positive_feature_gradients, negative_feature_gradients, feature_value = FeatureGradients(image=initial_image, 
+                                                                                                    feature_index=feature_index,
+                                                                                                    feature_layer=feature_layer)
 
-            grads_tensor = feature_gradients.numpy()
-            grads_matrix = grads_tensor[0, :, :, 0]
+            pos_grads_tensor = positive_feature_gradients.numpy()
+            pos_grads_matrix = pos_grads_tensor[0, :, :, 0]
+
+            neg_grads_tensor = negative_feature_gradients.numpy()
+            neg_grads_matrix = neg_grads_tensor[0, :, :, 0]
 
             value_scalar = feature_value.numpy()
-
+      
             thv, filter_img = cv2.threshold(raw_image, 55, 255, cv2.THRESH_TOZERO)
-            filtered_matrix = np.multiply(grads_matrix, filter_img)
+            filtered_pos_matrix = np.multiply(pos_grads_matrix, filter_img)
+            filtered_neg_matrix = np.multiply(neg_grads_matrix, filter_img)
 
-            bgr_sal_map = cv2.merge((raw_image, filtered_matrix, empty_img))
+            saliency_map = cv2.merge((neg_grads_matrix, raw_image, pos_grads_matrix))
+            filtered_map = cv2.merge((filtered_neg_matrix, raw_image, filtered_pos_matrix)) 
+
+            resized_saliency_map = cv2.resize(saliency_map,(1024,1024))
+            resized_filtered_map = cv2.resize(filtered_map,(1024,1024))
+
+            bordered_sal = cv2.copyMakeBorder(resized_saliency_map, top=4, bottom=4, left=4, right=2, borderType=cv2.BORDER_CONSTANT, value=255)
+            bordered_fil = cv2.copyMakeBorder(resized_filtered_map, top=4, bottom=4, left=2, right=4, borderType=cv2.BORDER_CONSTANT, value=255)
+            
+            both = np.concatenate((bordered_sal, bordered_fil), axis=1)
 
             ind = str(feature_index + 1).zfill(nulls)
 
-            cv2.imwrite(img_out_path + output_layer_name + '_image_[' + img_name + ']_feature_maps_' + str(value_scalar) + '_' + ind + '.png', bgr_sal_map)
+            cv2.imwrite(img_out_path + output_layer_name + '_image_[' + img_name + ']_feature_maps_' + str(value_scalar) + '_' + ind + '.png', both)
 
 gap_saliency_map = FilterBackpropSaliencyMapping(model_load_path='/saved/model/directory/model.h5',
                                                  img_out_path='/mapped/to/images/activations/directory/',
