27f3e22551a26e252b54d8a8a7402bbb0be9597c
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2020-04-16 15:32:05 +0200

    Filters Visualisation v2.4.0 example (tf. v1.14)

--- Visualisation/ConvLayerFilters_v2.4.0.py
+++ Visualisation/ConvLayerFilters_v2.4.0.py
@@ -12,34 +12,41 @@ def ConvLayerFilters(model_load_path, img_out_path, layer_name):
     conv_layer_weights = conv_layer.get_weights()
 
     num_of_filters = conv_layer_weights[0].shape[-1]
+    num_of_channels = conv_layer_weights[0].shape[-2]
 
-    nulls = len(str(num_of_filters))
+    filters_nulls = len(str(num_of_filters))
+    channel_nulls = len(str(num_of_channels))
 
-    print(layer_name, ' filters  to  visualise: ', num_of_filters)
+    print(layer_name, ' filters  to  visualise: ', num_of_channels * num_of_filters)
 
     filter_index = 0
 
-    while filter_index < num_of_filters:
-
-        filter_matrix = conv_layer_weights[0][:, :, 0, filter_index] #at the 0th channel
-
-        normalized = cv2.normalize(filter_matrix, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F).astype(np.uint8)
-
-        resized = cv2.resize(normalized,(256,256))
-
-        blur = cv2.GaussianBlur(resized,(5,5),0)
-
-        retv, ots = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    for filter_ind in tqdm(range(num_of_filters), desc='Filters visualisation:', leave=True):
 
         filter_index += 1
-        
-        ind = str(filter_index).zfill(nulls)
 
-        cv2.imwrite(img_out_path + layer_name + '_filter_' + ind + '.png', ots)
+        channel_index = 0
 
-        print('%.2f%%'%(100*filter_index/num_of_filters), end="\r")
+        for channel_idx in tqdm(range(num_of_channels), desc='inner-loop:', leave=False):
 
-    print(layer_name, ' filters  visualised: ', filter_index)
+            filter_matrix = conv_layer_weights[0][:, :, channel_idx, filter_ind]
+
+            normalized = cv2.normalize(filter_matrix, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F).astype(np.uint8)
+
+            resized = cv2.resize(normalized,(256, 256))
+
+            blur = cv2.GaussianBlur(resized,(5, 5),0)
+
+            retv, ots = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+
+            channel_index += 1
+
+            ind = str(filter_index).zfill(filters_nulls)
+            idx = str(channel_index).zfill(channel_nulls)
+
+            cv2.imwrite(img_out_path + layer_name + '_filters_' + ind + '_' + idx + '.png', ots)
+
+    print(layer_name, ' filters  visualised: ', filter_index * channel_index)
 
 named_conv_layer_filter_weights_visualised = ConvLayerFilters(model_load_path='/saved/model/directory/model.h5',
                                                               img_out_path='/visualised/filters/directory/',
