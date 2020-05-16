f224d7ee7ba9caa9d55760ce64693668ec918041
Ma. Marcinowski <39803228+Ma-Marcinowski@users.noreply.github.com>
2020-05-16 22:57:14 +0200

    Preprocessing v0.7

--- Preprocessing/Dataframe_v0.7.py
+++ Preprocessing/Dataframe_v0.7.py
@@ -25,7 +25,7 @@ def Dataframe(mode, img_path, df_path, df_img_path, df_parts):
         for j in tqdm(pngs, leave=False):
             for i in pngs:
 
-                if j[:8] == i[:8] and (j, i) not in rev:
+                if j[:8] == i[:8] and j[0] == i[0] and (j, i) not in rev:
 
                     pair = [df_img_path + j, df_img_path + i, 1]
 
@@ -41,39 +41,16 @@ def Dataframe(mode, img_path, df_path, df_img_path, df_parts):
 
         print('Done ' + mode + ' positives: ', pos, ' instances.')
 
-        if mode == 'test' or mode == 'train':
+        if mode == 'train' or mode == 'validation':
 
             neg = 0
 
-            for j in tqdm(pngs, leave=False):
-                for i in pngs:
-
-                    if j[:8] != i[:8] and (j, i) not in rev:
-
-                        pair = [df_img_path + j, df_img_path + i, 0]
-
-                        writer.writerow(pair)
-
-                        neg += 1
-
-                        rev.add((i, j))
-
-                    else:
-
-                        continue
-
-            print('Done ' + mode + ' negatives: ', neg, ' instances.')
-
-        elif mode == 'validation':
-
-            neg = 0
-
-            while neg < pos:
+            while neg < (pos * 10):
 
                 j = random.choice(pngs)
                 i = random.choice(pngs)
 
-                if j[:8] != i[:8] and (j, i) not in rev:
+                if j[:8] != i[:8] and j[0] == i[0] and (j, i) not in rev:
 
                     pair = [df_img_path + j, df_img_path + i, 0]
 
@@ -92,6 +69,29 @@ def Dataframe(mode, img_path, df_path, df_img_path, df_parts):
             else:
 
                 print('Done ' + mode + ' negatives: ', neg, ' instances.')
+
+        elif mode == 'test':
+
+            neg = 0
+
+            for j in tqdm(pngs, leave=False):
+                for i in pngs:
+
+                    if j[:8] != i[:8] and j[0] == i[0] and (j, i) not in rev:
+
+                        pair = [df_img_path + j, df_img_path + i, 0]
+
+                        writer.writerow(pair)
+
+                        neg += 1
+
+                        rev.add((i, j))
+
+                    else:
+
+                        continue
+
+            print('Done ' + mode + ' negatives: ', neg, ' instances.')
 
     df = pd.read_csv(df_path, header=None)
 
@@ -113,7 +113,7 @@ def Dataframe(mode, img_path, df_path, df_img_path, df_parts):
 
             p.dropna(axis=0, how='any', inplace=True)
 
-            print('Done train dataframe part ', idx, ': ', p.shape[0], ' image pairs.')
+            print('Done ' + mode + ' dataframe part ', idx, ': ', p.shape[0], ' image pairs.')
 
         print('Done splitting ' + mode + ' dataframes.')
 
